609c2932a92fb907e8beff5db32fb75001d890d7
damiancabana <damian-cr@live.com.ar>
2018-04-13 22:10:33 -0300

    modifico el testCliente

--- src/test/java/sge/testCliente.java
+++ src/test/java/sge/testCliente.java
@@ -13,54 +13,41 @@ public class testCliente {
 	public void init() {
 		this.unCliente = new Cliente();
 		this.unCliente.setNombre("Pepe");
-	
-	}
-
-	@Test
-	public void testAlgunoEncendido() {
 		ArrayList <Dispositivo> auxDispositivos;
 		auxDispositivos = new ArrayList<Dispositivo>();
 		auxDispositivos.add(new Dispositivo("TV",1));
 		auxDispositivos.add(new Dispositivo("Heladera",2));
 		auxDispositivos.add(new Dispositivo("Plancha",3));
-		
-		auxDispositivos.get(0).setEstaEncendido(true);
-		auxDispositivos.get(1).setEstaEncendido(false);
-		
 		this.unCliente.setDispositivos(auxDispositivos);
+	
+	}
+
+	@Test
+	public void testAlgunoEncendido() {
+		ArrayList <Dispositivo> dispositivos = this.unCliente.getDispositivos();
 		
-		Assert.assertEquals(1, this.unCliente.cantidadEncendidos(), 0.01);
+		dispositivos.get(0).setEstaEncendido(true);
+		dispositivos.get(1).setEstaEncendido(false);
+		dispositivos.get(2).setEstaEncendido(true);
+		
+		Assert.assertEquals(2, this.unCliente.cantidadEncendidos(), 0.01);
 		assertTrue (this.unCliente.algunoEncendido());
 	}
 	
 	
 	@Test
 	public void testCantidadApagados() {
-		ArrayList <Dispositivo> auxDispositivos;
-		auxDispositivos = new ArrayList<Dispositivo>();
-		auxDispositivos.add(new Dispositivo("TV",1));
-		auxDispositivos.add(new Dispositivo("Heladera",2));
-		auxDispositivos.add(new Dispositivo("Plancha",3));
+		ArrayList <Dispositivo> dispositivos = this.unCliente.getDispositivos();
 		
-		auxDispositivos.get(0).setEstaEncendido(false);
-		auxDispositivos.get(1).setEstaEncendido(false);
-		auxDispositivos.get(2).setEstaEncendido(false);
-		
-		this.unCliente.setDispositivos(auxDispositivos);
+		dispositivos.get(0).setEstaEncendido(false);
+		dispositivos.get(1).setEstaEncendido(false);
+		dispositivos.get(2).setEstaEncendido(false);
 		
 		Assert.assertEquals(3, this.unCliente.cantidadApagados(), 0.01);
 	}
 	
 	@Test
 	public void testAgregarDispositivos() {
-		ArrayList <Dispositivo> auxDispositivos;
-		auxDispositivos = new ArrayList<Dispositivo>();
-		auxDispositivos.add(new Dispositivo("TV",1));
-		auxDispositivos.add(new Dispositivo("Heladera",2));
-		auxDispositivos.add(new Dispositivo("Plancha",3));
-		
-		this.unCliente.setDispositivos(auxDispositivos);
-		
 		Assert.assertEquals(3, this.unCliente.cantidadDispositivos(), 0.01);
 		
 		this.unCliente.agregarDispositivo(new Dispositivo("Ventilador",4));
